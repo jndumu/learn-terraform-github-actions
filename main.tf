29d5982159322f1ef553c10691690868ad300590
[josie-at-944809923028 <fetinue3@gmail.com>
2022-07-14 20:46:19 -0400

    Point backend to correct josie organization and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "Josie-Terraform-Demos"
+    organization = "Josie-Terraform-Demo"
 
     workspaces {
       name = "josie-github-action"
